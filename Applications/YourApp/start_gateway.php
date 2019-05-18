a5e39f11cae9e63964a829d9e52e40ec8b122e17
zhaotianhang <1246223199@qq.com>
2019-05-18 19:19:21 +0800

    update_4

--- Applications/YourApp/start_gateway.php
+++ Applications/YourApp/start_gateway.php
@@ -25,9 +25,9 @@ $gateway = new Gateway("tcp://0.0.0.0:8080");
 // gateway名称，status方便查看
 $gateway->name = 'YourAppGateway';
 // gateway进程数
-$gateway->count = 1;
+$gateway->count = 4;
 // 本机ip，分布式部署时使用内网ip
-$gateway->lanIp = '192.168.0.240';
+$gateway->lanIp = '127.0.0.1';
 // 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
 // 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口 
 $gateway->startPort = 2900;
