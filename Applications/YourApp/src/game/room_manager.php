007468460e12563c22e9a32c2394dde5449b75e5
labanlei <labanlei@126.com>
2019-07-04 10:48:38 +0800

    房间管理2

--- Applications/YourApp/src/game/room_manager.php
+++ Applications/YourApp/src/game/room_manager.php
@@ -4,6 +4,7 @@ use GatewayWorker\Lib\Gateway;
 use Events;
 use room_base;
 use user;
+use mongo_db;
 
 //global
 
@@ -39,20 +40,54 @@ class room_manager{
     //创建房间
     private function create_room($game_type)
     {
+        $collname='gmae_Competition';
+        $mongodb=mongo_db::singleton('func_system');
+
         switch ($game_type)
         {
             case room_base::$room_type[0]:
-                $tmproom=new room_base();
-                $tmproom->set_code(time());
-                $tmproom->set_max(3);
-                self::$rooms[$tmproom->get_code()]=$tmproom;
+                $filter = [
+                    "type" => ['$eq' => room_base::$room_type[0]]//查询条件 type 等于 ddz
+                ];
+                $queryWriteOps = [
+                    "projection" => ["_id"=> 0],//不输出_id字段
+                    "sort"       => ["id" => 1],//根据id字段排序 1是升序，-1是降序
+                    "limit"      => 1
+                ];
+                $rs = $mongodb->query($collname, $filter, $queryWriteOps);
+                for ($i=0;$i<$rs[0]['number'];$i++){
+                    $tmproom=new room_base();
+                    $tmproom->set_code(time());
+                    $tmproom->set_max(3);
+                    self::$rooms[$tmproom->get_code()]=$tmproom;
+                }
                 break;
             case room_base::$room_type[1]:
-                $tmproom=new room_base();
-                $tmproom->set_code(time());
-                $tmproom->set_max(4);
-                self::$rooms[$tmproom->get_code()] = $tmproom;
+                $filter = [
+                    "type" => ['$eq' => room_base::$room_type[1]]//查询条件 type 等于 xzdd
+                ];
+                $queryWriteOps = [
+                    "projection" => ["_id"=> 0],//不输出_id字段
+                    "sort"       => ["id" => 1],//根据id字段排序 1是升序，-1是降序
+                    "limit"      => 1
+                ];
+                $rs = $mongodb->query($collname, $filter, $queryWriteOps);
+                for ($i=0;$i<$rs[0]['number'];$i++){
+                    $tmproom=new room_base();
+                    $tmproom->set_code(time());
+                    $tmproom->set_max(4);
+                    self::$rooms[$tmproom->get_code()] = $tmproom;
+                }
                 break;
+        }
+    }
+
+    //获取可用房间
+    private function get_empty_room(){
+        foreach (self::$rooms as $room){
+            if ($room->get_state()<=1){
+                return $room;
+            }
         }
     }
 
