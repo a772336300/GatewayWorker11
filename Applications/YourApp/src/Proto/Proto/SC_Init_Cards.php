5d02d7810f695334dfb8d5c2e31bdc12fd8f557f
labanlei <labanlei@126.com>
2019-07-15 14:43:52 +0800

    proto

---
+++ Applications/YourApp/src/Proto/Proto/SC_Init_Cards.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Auto generated from doudizhu.proto at 2019-07-15 14:34:25
+ *
+ * proto package
+ */
+
+namespace Proto {
+/**
+ * SC_Init_Cards message
+ */
+class SC_Init_Cards extends \ProtobufMessage
+{
+    /* Field index constants */
+    const CARDS = 1;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::CARDS => array(
+            'name' => 'cards',
+            'required' => false,
+            'type' => \ProtobufMessage::PB_TYPE_STRING,
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::CARDS] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'cards' property
+     *
+     * @param string $value Property value
+     *
+     * @return null
+     */
+    public function setCards($value)
+    {
+        return $this->set(self::CARDS, $value);
+    }
+
+    /**
+     * Returns value of 'cards' property
+     *
+     * @return string
+     */
+    public function getCards()
+    {
+        $value = $this->get(self::CARDS);
+        return $value === null ? (string)$value : $value;
+    }
+
+    /**
+     * Returns true if 'cards' property is set, false otherwise
+     *
+     * @return boolean
+     */
+    public function hasCards()
+    {
+        return $this->get(self::CARDS) !== null;
+    }
+}
+}
---
+++ Applications/YourApp/src/Proto/Proto/SC_Is_Gaming.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Auto generated from doudizhu.proto at 2019-07-15 14:34:25
+ *
+ * proto package
+ */
+
+namespace Proto {
+/**
+ * SC_Is_Gaming message
+ */
+class SC_Is_Gaming extends \ProtobufMessage
+{
+    /* Field index constants */
+    const DATA = 1;
+
+    /* @var array Field descriptors */
+    protected static $fields = array(
+        self::DATA => array(
+            'name' => 'data',
+            'required' => false,
+            'type' => \ProtobufMessage::PB_TYPE_INT,
+        ),
+    );
+
+    /**
+     * Constructs new message container and clears its internal state
+     */
+    public function __construct()
+    {
+        $this->reset();
+    }
+
+    /**
+     * Clears message values and sets default ones
+     *
+     * @return null
+     */
+    public function reset()
+    {
+        $this->values[self::DATA] = null;
+    }
+
+    /**
+     * Returns field descriptors
+     *
+     * @return array
+     */
+    public function fields()
+    {
+        return self::$fields;
+    }
+
+    /**
+     * Sets value of 'data' property
+     *
+     * @param integer $value Property value
+     *
+     * @return null
+     */
+    public function setData($value)
+    {
+        return $this->set(self::DATA, $value);
+    }
+
+    /**
+     * Returns value of 'data' property
+     *
+     * @return integer
+     */
+    public function getData()
+    {
+        $value = $this->get(self::DATA);
+        return $value === null ? (integer)$value : $value;
+    }
+
+    /**
+     * Returns true if 'data' property is set, false otherwise
+     *
+     * @return boolean
+     */
+    public function hasData()
+    {
+        return $this->get(self::DATA) !== null;
+    }
+}
+}
