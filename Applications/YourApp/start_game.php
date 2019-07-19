197f0d1f82f345648a953b321d37a8338299c2c2
zhaotianhang <1246223199@qq.com>
2019-07-19 16:05:40 +0800

    game_proto

--- Applications/YourApp/start_game.php
+++ Applications/YourApp/start_game.php
@@ -43,47 +43,47 @@ require_once 'src_game/redis.php';
 require_once 'src_game/send_pack.php';
 require_once 'src_game/proto_arr.php';
 // bussinessWorker 进程
-$tcp_worker = new BusinessWorker();
+$game_worker = new BusinessWorker();
 // worker名称
-$tcp_worker->eventHandler='Events_Game';
-$tcp_worker->name = 'game_worker';
+$game_worker->eventHandler='Events_Game';
+$game_worker->name = 'game_worker';
 // bussinessWorker进程数量
-$tcp_worker->count = 1;
+$game_worker->count = 1;
 // 服务注册地址
-$tcp_worker->registerAddress = '127.0.0.1:1238';
+$game_worker->registerAddress = '127.0.0.1:1239';
 
 
 
-$tcp_worker->product_uid_count = 0;
-//连接数
-$connection_count = 0;
-
+$game_worker->product_uid_count = 0;
 
 //设置db
-$tcp_worker->db = null;
+$game_worker->db = null;
 //设置db_web
-//$tcp_worker->db_web = null;
+//$game_worker->db_web = null;
 //设置任务服务器连接句柄
-$tcp_worker->taskServerConnections = array();
+$game_worker->taskServerConnections = array();
 //连接数缓存
-$tcp_worker->taskServerConnectionsCount=0;
+$game_worker->taskServerConnectionsCount=0;
 //下次发送到任务服务器目标
-$tcp_worker->taskServerConnectionsIndex=0;
-$test_count=0;
-$test_count1=0;
+$game_worker->taskServerConnectionsIndex=0;
 $redis = null;
 $monodb= null;
 //worker进程开始
-$tcp_worker->onWorkerStart = function ($worker)
+$game_worker->onWorkerStart = function ($worker)
 {
-    global $tcp_worker;
+    global $cardModel;
+    global $game_worker;
     global $config;
     global $redis;
     //初始化数据库
-    $tcp_worker->db= new \Workerman\MySQL\Connection($config['db']['host'],$config['db']['port'],$config['db']['user'],$config['db']['password'],$config['db']['dbname'],$config['db']['charset']);
+    //$game_worker->db= new \Workerman\MySQL\Connection($config['db']['host'],$config['db']['port'],$config['db']['user'],$config['db']['password'],$config['db']['dbname'],$config['db']['charset']);
 
     $redis = new Redis();
     $redisCon=$redis->pconnect('127.0.0.1');
+    $redis->flushAll();
+    foreach ($cardModel as $item)
+        $redis->sAdd('cardsModel',$item);
+
    // $monodb = new MongoDB\Client();
     //$monodb= new MongoDB\Client('mongodb+srv://myTester:xyz123@127.0.0.1:27017/test?retryWrites=true&w=majority');
     //$monodb_test = $monodb->test;
@@ -91,7 +91,7 @@ $tcp_worker->onWorkerStart = function ($worker)
 
 
 // 新增加一个属性，用来保存uid到connection的映射
-$tcp_worker->uidConnections = array();
+$game_worker->uidConnections = array();
 
 // 如果不是在根目录启动，则运行runAll方法
 if(!defined('GLOBAL_START'))
