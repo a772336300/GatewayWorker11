62898f6badf73598ea02f213de3f432c7210f59a
labanlei <labanlei@126.com>
2019-09-02 10:33:58 +0800

    ！！！！！！！！

--- Applications/YourApp/src_game/switch.php
+++ Applications/YourApp/src_game/switch.php
@@ -112,5 +112,20 @@ function switch_game($client_id,$mid,$data)
                 echo sprintf("CS_ComPetition_Join competitionid = %s playerid = %s\n",$com_join->getCompetitionId(),$com_join->getPlayerId());
                 break;
             }
+        case Message_Id::CS_CreateCardRoom_Id:
+        {
+            $createroom = new \Proto\CS_CreateCardRoom();
+            $createroom->parseFromString($data);
+            $createroom->getPlayerid();
+            $createroom->getRoomType();
+            $createroom->getGameType();
+            $createroom->getPlayers();
+            $createroom->getNumberOfGames();
+            $createroom->getSignUpTime();
+            $createroom->getBeginningTime();
+            $createroom->getRoomName();
+            $createroom->getRoomExplain();
+            break;
+        }
     }
 }
