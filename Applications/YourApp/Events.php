498e0bda6b9c6f549b45a58c9c0c472217466bee
zhaotianhang <1246223199@qq.com>
2019-09-14 11:58:06 +0800

    xxxxxxxx

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -61,14 +61,10 @@ class Events
    {
        //echo substr($packet,0,4);
        // 向客户端发送hello $data
-//       if(unpack('i*',substr($packet,0,4))[1]!=strlen($packet))
-//       {
-//           $test_count1++;
-//           echo $test_count1;
-//           echo "bad pack!";
-//           return ;
-//       }
-
+       if(unpack('i*',substr($packet,0,4))[1]!=strlen($packet))
+       {
+           return ;
+       }
        self::nianbao($packet,$client_id);
       // message_switch($client_id,unpack('i*',substr($packet,4,4))[1],substr($packet,8));
         // 向所有人发送 
