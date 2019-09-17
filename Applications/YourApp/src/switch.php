25d535232a60f038b2b113e864d074009d32490a
zhaotianhang <1246223199@qq.com>
2019-09-17 18:16:01 +0800

    xxxxxxxx

--- Applications/YourApp/src/switch.php
+++ Applications/YourApp/src/switch.php
@@ -404,7 +404,7 @@ function message_switch($client_id,$mid,$data)
                 $play_game_result = new \Proto\CS_Game_Over_Score();
                 $play_game_result->parseFromString($data);
 
-//                db_add_user_game_store($_SESSION['uid'],$play_game_result);
+                db_add_user_game_store($_SESSION['uid'],$play_game_result);
 //                task_manager($mid);
                 task_udpate_game($play_game_result,$_SESSION['uid']);
                 break;
