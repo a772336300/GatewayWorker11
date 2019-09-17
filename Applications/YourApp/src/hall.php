45eee02f85ae185220503ad65af76b8d5c72be31
happier9002 <124442296@qq.com>
2019-09-17 13:09:03 +0800

    12321

--- Applications/YourApp/src/hall.php
+++ Applications/YourApp/src/hall.php
@@ -275,10 +275,10 @@ function hall_message_switch($mid,$data){
             break;
         //购买商品
         case 20017:
-//            $goods = new \Proto\CS_User_Buy_Goods();
-//            $goods->parseFromString($data);
-//            $_id=$goods->getId();
-            $_id='5d79e7d0e8b270500488ef3f';
+            $goods = new \Proto\CS_User_Buy_Goods();
+            $goods->parseFromString($data);
+            $_id=$goods->getId();
+//            $_id='5d79e7d0e8b270500488ef3f';
             $collname="prop_config";
             $filter = [
                 "_id" => new ObjectId($_id)
