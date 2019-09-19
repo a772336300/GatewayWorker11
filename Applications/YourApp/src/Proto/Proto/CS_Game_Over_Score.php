df56b0464911ac9e0cc96d3389d1446a0da73edc
happier9002 <124442296@qq.com>
2019-09-19 18:01:50 +0800

    12321

--- Applications/YourApp/src/Proto/Proto/CS_Game_Over_Score.php
+++ Applications/YourApp/src/Proto/Proto/CS_Game_Over_Score.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Auto generated from new.proto at 2019-08-01 15:29:41
+ * Auto generated from new.proto at 2019-09-19 10:53:53
  *
  * proto package
  */
@@ -12,49 +12,13 @@ namespace Proto {
 class CS_Game_Over_Score extends \ProtobufMessage
 {
     /* Field index constants */
-    const GAMEID = 1;
-    const VALUE1 = 2;
-    const VALUE2 = 3;
-    const VALUE3 = 4;
-    const VALUE4 = 5;
-    const VALUE5 = 6;
-    const VALUE6 = 7;
+    const VALUE = 1;
 
     /* @var array Field descriptors */
     protected static $fields = array(
-        self::GAMEID => array(
-            'name' => 'gameid',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE1 => array(
-            'name' => 'value1',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE2 => array(
-            'name' => 'value2',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE3 => array(
-            'name' => 'value3',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE4 => array(
-            'name' => 'value4',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE5 => array(
-            'name' => 'value5',
-            'required' => false,
-            'type' => \ProtobufMessage::PB_TYPE_INT,
-        ),
-        self::VALUE6 => array(
-            'name' => 'value6',
-            'required' => false,
+        self::VALUE => array(
+            'name' => 'value',
+            'repeated' => true,
             'type' => \ProtobufMessage::PB_TYPE_INT,
         ),
     );
@@ -74,13 +38,7 @@ class CS_Game_Over_Score extends \ProtobufMessage
      */
     public function reset()
     {
-        $this->values[self::GAMEID] = null;
-        $this->values[self::VALUE1] = null;
-        $this->values[self::VALUE2] = null;
-        $this->values[self::VALUE3] = null;
-        $this->values[self::VALUE4] = null;
-        $this->values[self::VALUE5] = null;
-        $this->values[self::VALUE6] = null;
+        $this->values[self::VALUE] = array();
     }
 
     /**
@@ -94,234 +52,77 @@ class CS_Game_Over_Score extends \ProtobufMessage
     }
 
     /**
-     * Sets value of 'gameid' property
+     * Appends value to 'value' list
      *
-     * @param integer $value Property value
-     *
-     * @return null
-     */
-    public function setGameid($value)
-    {
-        return $this->set(self::GAMEID, $value);
-    }
-
-    /**
-     * Returns value of 'gameid' property
-     *
-     * @return integer
-     */
-    public function getGameid()
-    {
-        $value = $this->get(self::GAMEID);
-        return $value === null ? (integer)$value : $value;
-    }
-
-    /**
-     * Returns true if 'gameid' property is set, false otherwise
-     *
-     * @return boolean
-     */
-    public function hasGameid()
-    {
-        return $this->get(self::GAMEID) !== null;
-    }
-
-    /**
-     * Sets value of 'value1' property
-     *
-     * @param integer $value Property value
+     * @param integer $value Value to append
      *
      * @return null
      */
-    public function setValue1($value)
+    public function appendValue($value)
     {
-        return $this->set(self::VALUE1, $value);
+        return $this->append(self::VALUE, $value);
     }
 
     /**
-     * Returns value of 'value1' property
-     *
-     * @return integer
-     */
-    public function getValue1()
-    {
-        $value = $this->get(self::VALUE1);
-        return $value === null ? (integer)$value : $value;
-    }
-
-    /**
-     * Returns true if 'value1' property is set, false otherwise
-     *
-     * @return boolean
-     */
-    public function hasValue1()
-    {
-        return $this->get(self::VALUE1) !== null;
-    }
-
-    /**
-     * Sets value of 'value2' property
-     *
-     * @param integer $value Property value
+     * Clears 'value' list
      *
      * @return null
      */
-    public function setValue2($value)
+    public function clearValue()
     {
-        return $this->set(self::VALUE2, $value);
+        return $this->clear(self::VALUE);
     }
 
     /**
-     * Returns value of 'value2' property
+     * Returns 'value' list
      *
-     * @return integer
+     * @return integer[]
      */
-    public function getValue2()
+    public function getValue()
     {
-        $value = $this->get(self::VALUE2);
-        return $value === null ? (integer)$value : $value;
+        return $this->get(self::VALUE);
     }
 
     /**
-     * Returns true if 'value2' property is set, false otherwise
+     * Returns true if 'value' property is set, false otherwise
      *
      * @return boolean
      */
-    public function hasValue2()
+    public function hasValue()
     {
-        return $this->get(self::VALUE2) !== null;
+        return count($this->get(self::VALUE)) !== 0;
     }
 
     /**
-     * Sets value of 'value3' property
+     * Returns 'value' iterator
      *
-     * @param integer $value Property value
-     *
-     * @return null
+     * @return \ArrayIterator
      */
-    public function setValue3($value)
+    public function getValueIterator()
     {
-        return $this->set(self::VALUE3, $value);
+        return new \ArrayIterator($this->get(self::VALUE));
     }
 
     /**
-     * Returns value of 'value3' property
+     * Returns element from 'value' list at given offset
+     *
+     * @param int $offset Position in list
      *
      * @return integer
      */
-    public function getValue3()
+    public function getValueAt($offset)
     {
-        $value = $this->get(self::VALUE3);
-        return $value === null ? (integer)$value : $value;
+        return $this->get(self::VALUE, $offset);
     }
 
     /**
-     * Returns true if 'value3' property is set, false otherwise
+     * Returns count of 'value' list
      *
-     * @return boolean
+     * @return int
      */
-    public function hasValue3()
+    public function getValueCount()
     {
-        return $this->get(self::VALUE3) !== null;
-    }
-
-    /**
-     * Sets value of 'value4' property
-     *
-     * @param integer $value Property value
-     *
-     * @return null
-     */
-    public function setValue4($value)
-    {
-        return $this->set(self::VALUE4, $value);
-    }
-
-    /**
-     * Returns value of 'value4' property
-     *
-     * @return integer
-     */
-    public function getValue4()
-    {
-        $value = $this->get(self::VALUE4);
-        return $value === null ? (integer)$value : $value;
-    }
-
-    /**
-     * Returns true if 'value4' property is set, false otherwise
-     *
-     * @return boolean
-     */
-    public function hasValue4()
-    {
-        return $this->get(self::VALUE4) !== null;
-    }
-
-    /**
-     * Sets value of 'value5' property
-     *
-     * @param integer $value Property value
-     *
-     * @return null
-     */
-    public function setValue5($value)
-    {
-        return $this->set(self::VALUE5, $value);
-    }
-
-    /**
-     * Returns value of 'value5' property
-     *
-     * @return integer
-     */
-    public function getValue5()
-    {
-        $value = $this->get(self::VALUE5);
-        return $value === null ? (integer)$value : $value;
-    }
-
-    /**
-     * Returns true if 'value5' property is set, false otherwise
-     *
-     * @return boolean
-     */
-    public function hasValue5()
-    {
-        return $this->get(self::VALUE5) !== null;
-    }
-
-    /**
-     * Sets value of 'value6' property
-     *
-     * @param integer $value Property value
-     *
-     * @return null
-     */
-    public function setValue6($value)
-    {
-        return $this->set(self::VALUE6, $value);
-    }
-
-    /**
-     * Returns value of 'value6' property
-     *
-     * @return integer
-     */
-    public function getValue6()
-    {
-        $value = $this->get(self::VALUE6);
-        return $value === null ? (integer)$value : $value;
-    }
-
-    /**
-     * Returns true if 'value6' property is set, false otherwise
-     *
-     * @return boolean
-     */
-    public function hasValue6()
-    {
-        return $this->get(self::VALUE6) !== null;
+        return $this->count(self::VALUE);
     }
 }
 }
